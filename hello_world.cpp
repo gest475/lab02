625d37f23082bc2ae7a747bf52ad420b1ece2fab
alsxndrsps <arina.lisichkina@bk.ru>
2026-05-26 21:38:02 +0300

    Add user input and greeting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+    return 0;
+}#include <iostream>
 using namespace std;
 
 int main() {
