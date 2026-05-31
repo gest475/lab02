37d1bb320d080098673a03e273657c5a8e620ea8
gest475 <arina.lisichkina@bk.ru>
2026-05-31 16:46:36 +0300

    Add hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    cout << "Hello world" << endl;
     return 0;
 }
